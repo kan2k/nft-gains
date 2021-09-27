4c2da26f213caed28f57a1ee7d2804399aef4f84
kan <>
2021-09-27 23:24:31 +0800

    'draft'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
+import { NftList, NftListItem } from './components/nftListItem/nftListItem'
 
 function App() {
+  const [nftItems, setNftItems] = useState([])
+  const [test, setTest] = useState('')
+
+  const options = { method: 'GET' }
+
+  useEffect(() => {
+    fetchData('0xBDa5baFAA1776d0c8ea849b35B1E50Bf988E3B03')
+    async function fetchData(address) {
+      const response = await fetch(
+        `https://api.opensea.io/api/v1/assets?owner=${address}&limit=50`,
+        options,
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          setNftItems(response.assets)
+          console.log('fetchData Response: ', response)
+        })
+        .catch((err) => console.error(err))
+    }
+  }, [])
+  // console.log(nftItems.assets[0].name)
+  // console.log(nftItems.assets[0].last_sale.total_price)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="nft-items">
+          <NftList list={nftItems} />
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/nftListItem/nftListItem.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import './nftListItem.css'
+
+function NftList({ list }) {
+  {
+    console.log(list)
+    return (
+      <div className="nft-items">
+        {/* display books from the API */}
+        {list && (
+          <div className="nft-item">
+            {/* loop over the books */}
+            {list.map((nft, index) => (
+              <div key={index}>
+                <NftListItem
+                  name={nft.name}
+                  icon={nft.image_url}
+                  openseaLink={nft.permalink}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+function NftListItem({ name, icon, openseaLink }) {
+  return (
+    <>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={icon} height="200px" />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+            You bought at: {0} ETH / {0} USD
+            <br />
+            Current Floor Price: {0} ETH / {0} USD
+            <br />
+            Profit/Loss: {0} ETH / {0} USD
+          </Card.Text>
+          <Button variant="primary" href={openseaLink}>
+            View on OpenSea
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
+
+export { NftList }
